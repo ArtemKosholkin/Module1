4c514a70b6a73918dd6cd5d3f40f7e35c0d92776
ArtemKosholkin <kaa@sdexport.ru>
2025-05-26 12:20:29 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как вас зовут?");
+            string name=Console.ReadLine();
+            Console.WriteLine("Сколько вам лет?");
+            string age = Console.ReadLine();
+            Greet(name, age);
+            Console.ReadKey();
+        }
+
+        static void Greet (string name, string age)
+        {
+            Console.WriteLine("Здравствуйте, {0}!", name);
+            Console.WriteLine("Мне сообщили, что Вам {0}", age);
+        }
+
+    }
+}
